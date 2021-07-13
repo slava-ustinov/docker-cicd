98162aa71e6926432647085b5553edfc0fa54822
Slava Ustinov <slava.ustinov@gmail.com>
2021-07-13 11:36:19 +0300

    Create docker_creation.groovy

---
+++ basics/dsl/docker_creation.groovy
@@ -0,0 +1,23 @@
+job('NodeJS Docker example') {
+    scm {
+        git('https://github.com/slava-ustinov/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    steps {
+        dockerBuildAndPublish {
+            buildContext('./basics/)
+            repositoryName('slavaus/docker-nodejs-demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('Dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
